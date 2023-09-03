cfad133b25305b426d043ac5d042a583ce30e590
unknown <tranminhtri10213@gmail.com>
2023-09-03 11:22:34 +0700

    update checkbox messagebox

--- checkboxGUI/checkboxGUI/Form1.cs
+++ checkboxGUI/checkboxGUI/Form1.cs
@@ -16,5 +16,30 @@ namespace checkboxGUI
         {
             InitializeComponent();
         }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("Checked");
+
+        }
+
+        private void checkBox1_CheckStateChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("CheckState");
+
+        }
+
+        private void checkBox1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Click");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = textBox1.Text;
+            string isRobot = checkBox1.CheckState == CheckState.Checked ? "Khong phai la " : checkBox1.CheckState == CheckState.Unchecked ? "la ":"phai cung khong phai";
+            string showString = string.Format("Chao ban {0},ban {1} robot", name, isRobot);
+            MessageBox.Show(showString);
+        }
     }
 }
